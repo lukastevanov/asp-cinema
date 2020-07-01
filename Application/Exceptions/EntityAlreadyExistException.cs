3ffbe2cc5c0f1fdb15e0ab371e1902fa5e2b0e10
lukastevanov <51883793+lukastevanov@users.noreply.github.com>
2020-07-01 16:13:00 +0200

    Add files via upload

---
+++ Application/Exceptions/EntityAlreadyExistException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Application.Exceptions
+{
+    public class EntityAlreadyExistException : Exception
+    {
+        public EntityAlreadyExistException() { }
+        public EntityAlreadyExistException(int id, Type type)
+            : base($"Entity of type {type.Name} with an id {id} already exist") { }
+    }
+}
---
+++ Application/Exceptions/EntityNotFoundException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Application.Exceptions
+{
+    public class EntityNotFoundException : Exception
+    {
+        public EntityNotFoundException()
+        {
+        }
+
+        public EntityNotFoundException(int id, Type type):base($"Entity of type {type.Name} with an id of {id} was not found."){
+        
+        }
+    }
+}
